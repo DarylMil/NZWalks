d819966ebf3588691983a54eed8ff6281ec1d400
42Daryl <62588147+42Daryl@users.noreply.github.com>
2022-09-03 18:18:13 +0800

    End of Session 3

---
+++ NZWalks/NZWalks.API/Profiles/RegionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionProfile:Profile
+    {
+        public RegionProfile()
+        {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>().ForMember(dest => dest.Id,
+                options => options.MapFrom(src => src.Id));
+
+        }
+    }
+}
